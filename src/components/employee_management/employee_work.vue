77a734cdb97c5d28f2a5dc094596b2f41cece0b2
chendou <1427760246@qq.com>
2022-01-04 21:22:34 +0800

    完善

--- src/components/employee_management/employee_work.vue
+++ src/components/employee_management/employee_work.vue
@@ -16,11 +16,11 @@
   <el-table :data="tableData"  style="width: 100%"
             :header-cell-style="{textAlign: 'center',background:'#f8f8f9',color:'#6C6C6C'}"
             :cell-style="{textAlign: 'center'}">
-    <el-table-column prop="date" label="姓名" width="210" />
-    <el-table-column prop="name" label="开始时间" width="210" />
-    <el-table-column prop="name" label="结束时间" width="210" />
-    <el-table-column prop="name" label="任职公司" width="210" />
-    <el-table-column prop="name" label="职位" width="210" />
+    <el-table-column prop="staffName" label="姓名" width="210" />
+    <el-table-column prop="workStareTime" label="开始时间" width="210" />
+    <el-table-column prop="createdTime" label="结束时间" width="210" />
+    <el-table-column prop="companyName" label="任职公司" width="210" />
+    <el-table-column prop="positionName" label="职位" width="210" />
 <!--    <el-table-column prop="name" label="离职原因" width="180" />-->
     <el-table-column fixed="right" label="操作">
     <template #default>
@@ -43,8 +43,8 @@
         :total="pageInfo.total"
         :pager-count="5"
         background
-        @size-change="selectUsers"
-        @current-change="selectUsers"
+        @size-change="selectWork"
+        @current-change="selectWork"
     >
     </el-pagination>
   </div>
@@ -56,28 +56,7 @@ export default {
   data() {
     return {
       information:'/employee/message/employee_roster/information',
-      tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-      ],
+      tableData: [],
       pageInfo: {
         // 分页参数
         currentPage: 1, //当前页
@@ -87,6 +66,26 @@ export default {
       input3:"",
     }
   },
+  methods:{
+
+    //多表查询
+    selectWork() {
+      this.axios
+          .get("http://localhost:8010/provider/workExperience/selectWorkExperience/"+this.pageInfo.currentPage+"/"+this.pageInfo.pagesize)
+          .then((response) => {
+            console.log(response);
+            this.tableData = response.data.data.records;
+            console.log(response.data.data.records)
+            this.pageInfo.total = response.data.data.total;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    },
+  },
+  created() {
+    this.selectWork()
+  }
 }
 
 </script>
